3b96512bbc2c7150e72a4a2cbb0ef825f8495d4b
Josh Ferrell <josh@joshferrell.me>
2019-06-03 18:22:34 -0700

    feat: added basic tailwind defaults

--- src/config/box-styles.ts
+++ src/config/box-styles.ts
@@ -1,16 +1,78 @@
 export default {
-    backgroundColor: {},
-    backgroundPosition: {},
-    backgroundSize: {},
-    boxShadow: {},
-    cursor: {},
-    inset: {},
-    listStyleType: {},
-    opacity: {},
-    stroke: {},
-    borderColor: {},
-    borderRadius: {},
-    borderWidth: {},
-    fill: {},
-    container: {}
+    backgroundPosition: {
+        bottom: 'bottom',
+        center: 'center',
+        left: 'left',
+        'left-bottom': 'left bottom',
+        'left-top': 'left top',
+        right: 'right',
+        'right-bottom': 'right bottom',
+        'right-top': 'right top',
+        top: 'top'
+    },
+    backgroundSize: {
+        auto: 'auto',
+        cover: 'cover',
+        contain: 'contain'
+    },
+    boxShadow: {
+        default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+        outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+        none: 'none'
+    },
+    cursor: {
+        auto: 'auto',
+        default: 'default',
+        pointer: 'pointer',
+        wait: 'wait',
+        text: 'text',
+        move: 'move',
+        'not-allowed': 'not-allowed'
+    },
+    inset: {
+        '0': '0',
+        auto: 'auto'
+    },
+    listStyleType: {
+        none: 'none',
+        disc: 'disc',
+        decimal: 'decimal'
+    },
+    opacity: {
+        '0': '0',
+      '25': '0.25',
+      '50': '0.5',
+      '75': '0.75',
+      '100': '1'
+    },
+    stroke: {
+        current: 'currentColor',
+        // TODO: color object
+    },
+    borderRadius: {
+        none: '0',
+      sm: '0.125rem',
+      default: '0.25rem',
+      lg: '0.5rem',
+      full: '9999px'
+    },
+    borderWidth: {
+        default: '1px',
+        '0': '0',
+        '2': '2px',
+        '4': '4px',
+        '8': '8px'
+    },
+    fill: {
+        current: 'currentColor',
+        // TODO: color object
+    }
 }
+
+// TODO: backgroundColor
+// TODO: borderColor
--- src/config/position.ts
+++ src/config/position.ts
@@ -1,8 +1,53 @@
 export default {
-    flex: {},
-    flexGrow: {},
-    flexShrink: {},
-    objectPosition: {},
-    order: {},
-    zIndex: {}
+    flex: {
+        '1': '1 1 0%',
+        auto: '1 1 auto',
+        initial: '0 1 auto',
+        none: 'none',
+    },
+    flexGrow: {
+        '0': '0',
+        default: '1',
+    },
+    flexShrink: {
+        '0': '0',
+        default: '1'
+    },
+    objectPosition: {
+        bottom: 'bottom',
+        center: 'center',
+        left: 'left',
+        'left-bottom': 'left bottom',
+        'left-top': 'left top',
+        right: 'right',
+        'right-bottom': 'right bottom',
+        'right-top': 'right top',
+        top: 'top',
+    },
+    order: {
+        first: '-9999',
+        last: '9999',
+        none: '0',
+        '1': '1',
+        '2': '2',
+        '3': '3',
+        '4': '4',
+        '5': '5',
+        '6': '6',
+        '7': '7',
+        '8': '8',
+        '9': '9',
+        '10': '10',
+        '11': '11',
+        '12': '12',
+    },
+    zIndex: {
+        auto: 'auto',
+        '0': '0',
+        '10': '10',
+        '20': '20',
+        '30': '30',
+        '40': '40',
+        '50': '50',
+    }
 }
